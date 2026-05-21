29beacfcd35bf6c0edead363ad1fb12a20dfc25f
Agnieszka Skowronska <askowronska.job@gmail.com>
2026-05-21 11:05:21 +0200

    Added AI_Foundry/OpenAI usage PoC.

---
+++ AI_PoCs/Source Code/AI_Foundry/AI_Foundry_Agent_SemanticKernel/GeneralPlugin.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace AI_Foundry_Agent_SemanticKernel;
+
+public class GeneralPlugin
+{
+    [KernelFunction]
+    [Description("Gets the current time.")]
+    public TimeSpan GetTime() => TimeProvider.System.GetLocalNow().TimeOfDay;
+
+    [KernelFunction]
+    [Description("Gets the current date.")]
+    public string GetDate() => TimeProvider.System.GetLocalNow().Date.ToLongDateString();
+}
---
+++ AI_PoCs/Source Code/AI_Foundry/AI_Foundry_Agent_SemanticKernel/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using AI_Foundry_Agent_SemanticKernel;
+
+var endpoint = Environment.GetEnvironmentVariable("OPENAI_ENDPOINT") ?? string.Empty;
+var key = Environment.GetEnvironmentVariable("OPENAI_KEY") ?? string.Empty;
+
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion("gpt-4o", endpoint, key);
+builder.Plugins.AddFromType<GeneralPlugin>();
+
+var kernel = builder.Build();
+
+var chatCompletionAgent = new ChatCompletionAgent
+{
+    Name = "TimeAgent",
+    Description = "Agent with the current date and time.",
+    Instructions = "You should only reply on questions related to the current date and time.",
+    Kernel = kernel,
+    Arguments = new KernelArguments(new OpenAIPromptExecutionSettings
+    {
+        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+    })
+};
+
+var agentThread = new ChatHistoryAgentThread();
+
+while (true)
+{
+    Console.Write("User:");
+    var request = Console.ReadLine();
+    agentThread.ChatHistory.AddUserMessage(request!);
+
+    string fullMessage = "";
+    Console.Write("Assistant:");
+
+    await foreach (var response in chatCompletionAgent.InvokeAsync(agentThread))
+    {
+        Console.Write(response.Message.Content);
+        fullMessage += response.Message.Content;
+    }
+
+    Console.WriteLine();
+
+    agentThread.ChatHistory.AddAssistantMessage(fullMessage);
+
+}
